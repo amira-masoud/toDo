6df14b26d90d57a775b4b0714f0f3d83eb0e6020
Amira <amira@Amiras-iMac.local>
2019-12-12 09:50:09 -0800

    first commit

---
+++ ToDoTask/ToDoTableViewController.swift
@@ -0,0 +1,120 @@
+//
+//  ToDoTableViewController.swift
+//  ToDoTask
+//
+//  Created by Amira on 12/12/19.
+//  Copyright © 2019 Amira. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+
+    @IBAction func addButton(_ sender: UIBarButtonItem) {
+       
+        let alert = UIAlertController(
+            title: "New to-do item",
+            message: "Insert the title of the new to-do item:",
+            preferredStyle: .alert)
+        
+        // Add a text field to the alert for the new item's title
+        alert.addTextField(configurationHandler: nil)
+        
+        // Add a "cancel" button to the alert. This one doesn't need a handler
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (UIAlertAction) in
+            
+            let title = alert.textFields?[0] as! UITextField
+            self.names.append(title.text!)
+            self.tableView.reloadData()
+        }))
+        
+    
+        
+         self.present(alert, animated: true, completion: nil)
+    }
+    
+    var names = ["A" , "M"]
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return names.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "Cell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+
+       cell.textLabel?.text = names[indexPath.row]
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    
+    let checkInAction = UIAlertAction(title: "Check in", style: .default, handler: {(action : UIAlertAction!) -> Void in
+        let cell = tableView.cellForRow(at : indexPath)
+        cell?.accessoryType = .checkmark
+    })
+    
+    }
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            names.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+        
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
